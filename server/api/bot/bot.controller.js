e2a61cf7478245fbefbdb47b3c630549c078e48c
Bernix01 <gbernal096@gmail.com>
2016-09-17 21:59:02 -0500

    should be enviroment controlled var

--- server/api/bot/bot.controller.js
+++ server/api/bot/bot.controller.js
@@ -13,11 +13,11 @@
 // Gets a list of Bots
 export function hook(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-     req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+     req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
    console.log("Validating webhook");
    return res.status(200).send(req.query['hub.challenge']);
  } else {
    console.error("Failed validation. Make sure the validation tokens match.");
-   return res.sendStatus(403);          
+   return res.sendStatus(403);
  }
 }
