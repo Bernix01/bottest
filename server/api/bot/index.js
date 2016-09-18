b6a799c7cc5c938cc9c75c91771a76e9d34d4f82
Bernix01 <gbernal096@gmail.com>
2016-09-17 21:57:11 -0500

    better hook it!

--- server/api/bot/index.js
+++ server/api/bot/index.js
@@ -5,11 +5,6 @@ var controller = require('./bot.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.upsert);
-router.patch('/:id', controller.patch);
-router.delete('/:id', controller.destroy);
+router.get('/', controller.hook);
 
 module.exports = router;
