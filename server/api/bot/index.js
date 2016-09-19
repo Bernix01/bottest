0c7d988785949d0ca98ff5a7ec10f17e52ca5cd0
Bernix01 <gbernal096@gmail.com>
2016-09-18 21:32:49 -0500

    bot?

--- server/api/bot/index.js
+++ server/api/bot/index.js
@@ -2,9 +2,11 @@
 
 var express = require('express');
 var controller = require('./bot.controller');
+const bodyParser = require('body-parser');
 
 var router = express.Router();
 
 router.get('/', controller.hook);
+router.post('/',bodyParser.json({ verify: controller.verifyRequestSignature }),controller.chat)
 
 module.exports = router;
